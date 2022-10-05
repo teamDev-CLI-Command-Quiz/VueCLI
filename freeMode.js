6070c6fbcb00fe7f0d349eeaba2c51f77966fe35
Hayato Koshi <kshyt2501@icloud.com>
2022-10-06 03:17:45 +0900

    10/6 スクロール、リアルタイム入力まで

--- freeMode.js
+++ freeMode.js
@@ -1,5 +1,5 @@
-let CLITextInput = document.getElementById("CLITextInput");
-let input = document.getElementById("input")
+let CLITextInputDiv = document.getElementById("CLITextInput");
+let content = document.getElementById("content")
 let CLITextOutputDiv = document.getElementById("CLIOutputDiv");
 
 const app = Vue.createApp({
@@ -8,112 +8,20 @@ const app = Vue.createApp({
     }),
     methods: {
         appendEchoParagraph:function(){
-            let CLITextOutput = this.CLITextInput
             CLITextOutputDiv.innerHTML += 
                 `
-                    <p class="m-0 output-text"> 
+                    <p class="m-0 output-text align-top"> 
                     <span>User</span>
                     <span>@</span>
-                    <span >UsernoMacBook-Pro % ${this.CLITextInput}
+                    <span>UsernoMacBook-Pro % ${this.CLITextInput}
                     </span>
                     </p>
                 `
                 // + CLITextOutputDiv.innerHTML;
             this.CLITextInput = ""
             console.log(CLITextOutputDiv.innerHTML)
-            CLITextOutputDiv.scrollTop = CLITextOutputDiv.scrollHeight;
+            content.scrollTo(0, content.scrollHeight);
         }
     }
 })
 app.mount('#input')
-
-// window.document.onkeydown = function(event){
-//     if (event.key === 'Enter') {
-//         CLITextOutputDiv.innerHTML = 
-//         `
-//         hello1
-//         `
-//         + CLITextOutputDiv.innerHTML;
-//         console.log(CLITextOutputDiv.innerHTML);
-//         return;
-//     }
-// }
-// app.component('appendText' ,{
-//     data() {
-//         return{
-//             CLITextInput: ""
-//         }
-//     },
-//     methods: {
-//         appendEchoParagraph:function(event, CLITextOutputDiv){
-//             CLITextOutputDiv.innerHTML += 
-//                 `
-//                 hello
-//                 `;
-//             console.log(parentDiv.innerHTML);
-//             return;
-//         }
-//     },
-//     template:
-//         `
-//         <p class="m-0 output-text"> 
-//         <span>User</span>
-//         <span>@</span>
-//         <span>UsernoMacBook-Pro</span> % {{ CLITextInput }}
-//         </p>
-//         `
-// }).mount('#input')
-
-// app.component('appendText' ,{
-//     data: function(event) {
-//         if (event.key == "Enter"){
-//             CLITextOutputDiv.innerHTML += `<appendText></appendText>`
-//             return
-//         }
-//     },
-//         template:         
-//         `
-//         <p class="m-0 output-text"> 
-//         <span>User</span>
-//         <span>@</span>
-//         <span>UsernoMacBook-Pro</span> % {{ CLITextInput }}
-//         </p>
-//         `
-//     })
-
-// document.onkeyup = submitSerch()
-// function submitSerch(event){
-//     if (event.key == "Enter"){
-//         appendEchoParagraph()
-//     }
-// }
-
-// submitSerch(event)
-// var data = {
-//     CLITextInput: ''
-// }
-// var vm = new Vue({ 
-//     el: '#input',
-//     data: data
-// })
-// Vue.component('appendText' ,{
-//     data: function(event) {
-//         if (event.key == "Enter"){
-//             console.log(true)
-//             return
-//         }
-//     },
-//     template:         
-//     `
-//     <p class="m-0 output-text"> 
-//     <span>User</span>
-//     <span>@</span>
-//     <span>UsernoMacBook-Pro</span> % {{ CLITextInput }}
-//     </p>
-//     `
-// })
-// vm.$data === data
-// vm.$el === document.getElementById("input")
-
-// new Vue({ el: '#input' })
-// CLITextInput.addEventListener("keyup", (event) => submitSerch(event));
